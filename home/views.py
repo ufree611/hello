217e2c0852a7cef116a1604943cbaf642e40805d
ufree611 <107075626+ufree611@users.noreply.github.com>
2022-06-08 02:10:07 +0500

    Initial commit

---
+++ home/views.py
@@ -0,0 +1,65 @@
+from multiprocessing import context
+from django.http import HttpResponse
+from django.shortcuts import render
+from home.models import contact
+from google_play_scraper import app
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+    #return HttpResponse("THis is this")
+def contact(request):
+    titles =[]
+    titlea = []
+    # if request.method == "POST":        
+    #     text2 = request.POST.get('text1')
+    #     text2 = text2.splitlines()
+    #     for x in text2:
+    #         result = app(
+    #             x,
+    #         )
+    #         title = result["title"]
+    #         version= result["version"]
+
+            # titles.append(version)
+            # titlea.append(version)
+    context = {
+                "title":titles,
+                "titles":titlea,
+            }
+    return render(request, 'contact.html', context) 
+#testing
+
+
+def testing(request):
+    if request.method == "GET":
+        url = request.GET['url']       
+        result = app(
+            url,
+        )
+        title = result["title"]
+        version = result["version"]
+        return HttpResponse(f"<td>{title}</td><td>{version}</td>")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def about(request):
+    x = "hello"
+    thhis = ["etc","et","e"]
+    for j in thhis:
+        print(thhis)
+    return HttpResponse("THis is about "+x)
