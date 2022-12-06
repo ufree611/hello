3c6b405e4c44494f50d006f98364ea02842fe6f9
ufree611 <107075626+ufree611@users.noreply.github.com>
2022-12-06 16:37:20 +0500

    hello

--- hello/settings.py
+++ hello/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zi5i9xmr=q9&t4$$(kui4do(@kqap#@a6iu%oh=_w_g*_*xmi6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['hellodone.herokuapp.com']
+ALLOWED_HOSTS = ['hellodone.herokuapp.com','127.0.0.1']
 
 
 # Application definition
